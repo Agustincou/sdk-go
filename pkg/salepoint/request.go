0c2748fc8535aeb4b00cd85f17818944a251805f
Juan Agustín Coutinho <cou_647@hotmail.com>
2024-04-18 12:48:03 -0300

    Fixes

--- pkg/salepoint/request.go
+++ pkg/salepoint/request.go
@@ -1,19 +1,19 @@
 package salepoint
 
 type CreateRequest struct {
-	Category        int    `json:"category"`
-	ExternalID      string `json:"external_id"`
-	ExternalStoreID string `json:"external_store_id"`
-	FixedAmount     bool   `json:"fixed_amount"`
-	Name            string `json:"name"`
-	StoreID         int    `json:"store_id"`
-}
-
-type SearchRequest struct {
-	UserID          int    `json:"user_id,omitempty"`
-	Name            string `json:"name,omitempty"`
 	Category        int    `json:"category,omitempty"`
-	StoreID         string `json:"store_id,omitempty"`
 	ExternalID      string `json:"external_id,omitempty"`
 	ExternalStoreID string `json:"external_store_id,omitempty"`
+	FixedAmount     bool   `json:"fixed_amount,omitempty"`
+	Name            string `json:"name,omitempty"`
+	StoreID         int    `json:"store_id,omitempty"`
+}
+
+// No agregar "omitempty" porque rompe la construcción de los queryParams
+type SearchRequest struct {
+	UserID          int    `json:"user_id"`
+	Category        int    `json:"category"`
+	StoreID         string `json:"store_id"`
+	ExternalID      string `json:"external_id"`
+	ExternalStoreID string `json:"external_store_id"`
 }
--- pkg/store/request.go
+++ pkg/store/request.go
@@ -1,25 +1,23 @@
 package store
 
 type BusinessHour struct {
-	Open  string `json:"open"`
-	Close string `json:"close"`
+	Open  string `json:"open,omitempty"`
+	Close string `json:"close,omitempty"`
 }
 
 type Location struct {
 	StreetNumber string  `json:"street_number"`
 	StreetName   string  `json:"street_name"`
-	CityName     string  `json:"city_name"`
-	StateName    string  `json:"state_name"`
-	Latitude     float64 `json:"latitude"`
-	Longitude    float64 `json:"longitude"`
-	Reference    string  `json:"reference"`
+	CityName     string  `json:"city_name,omitempty"`
+	StateName    string  `json:"state_name,omitempty"`
+	Latitude     float64 `json:"latitude,omitempty"`
+	Longitude    float64 `json:"longitude,omitempty"`
+	Reference    string  `json:"reference,omitempty"`
 }
 
 type CreateRequest struct {
-	BusinessHours map[string][]BusinessHour `json:"business_hours"`
-	ExternalID    string                    `json:"external_id"`
-	Location      Location                  `json:"location"`
-	Name          string                    `json:"name"`
+	BusinessHours map[string][]BusinessHour `json:"business_hours,omitempty"`
+	ExternalID    string                    `json:"external_id,omitempty"`
+	Location      Location                  `json:"location,omitempty"`
+	Name          string                    `json:"name,omitempty"`
 }
-
-
